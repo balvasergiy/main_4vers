04b38a917fa317572cdc745e6abc5f7e22adcaff
balvasergiy <balvasergiy@yandex.ru>
2024-09-05 22:57:40 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print(9**0.5*5) #1st program
+print(9.99>9.98 and 1000!=1000.1) #2nd program
+print(2*2+2) #3rd program
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+print(float("123.456"))#4th program
+print(123.456*10)
+print(int(1234.56))
+print(int(1234%10))
